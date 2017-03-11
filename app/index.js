5a1b74232675d7687dcd8d5dd82848d2e136a99f
CHARLOTTE <CHARLOTTE>
2017-03-11 14:45:51 +0100

    Created my main and tests

---
+++ app/index.js
@@ -0,0 +1,57 @@
+//this is my main code
+module.exports = {
+
+findMinMax: function (arr){
+    var min = Math.min(...arr);
+    var max = Math.max(...arr);
+    var result = [];
+    if(min !== max){
+        result.push(min);
+        result.push(max);
+    }else if(min === max){
+     result.push(min);
+     }
+        return result
+},
+
+fizzBuzz: function(n){
+	if(n % 3 === 0 && n % 5 === 0){
+		return ("FizzBuzz");
+	}
+	else{ 
+	  if (n % 3 === 0 && n % 5 !== 0){
+	  return ("Fizz");
+	  }
+	else{
+	  if (n % 3 !== 0 && n % 5 === 0){
+	   return ("Buzz");
+	  }
+	  else{
+	    if (n % 3 !== 0 && n % 5 !== 0){
+	      return (n);
+	    }
+	  }
+	}
+}
+},
+
+aritGeo: function(arr){
+    var first_term = arr[0];
+    var second_term = arr[1];
+    var comdif = arr[1] - arr[0];
+    var comrat = arr[1] / arr[0];
+    if(arr.length !== 0){
+    for(var i = 2; i <= arr.length; i++){
+        if(arr[i] - arr[i-1] === comdif){
+          return("Arithmetic");
+  } else if(arr[i] / arr[i-1] === comrat){
+    return ("Geometric");
+  } else if(arr[i] - arr[i-1] !== comdif && arr[i] / arr[i-1] !== comrat){
+    return -1;
+  }
+}
+}else{
+return 0;
+  }
+}
+}
